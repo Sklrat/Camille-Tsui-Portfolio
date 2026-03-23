8225c6236c7fee0627c450362103c5967a11a414
Camille Tsui <camille.tsui@edu.sait.ca>
2026-03-23 11:45:28 -0600

    Made navbar reusable

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,35 @@
 //nav bar
-let gamesClick = document.getElementById("games-button")
-let artClick = document.getElementById("art-button")
-let miscClick = document.getElementById("misc-bcutton")
+//load nav
+window.addEventListener("DOMContentLoaded", () => {
+    fetch ("../navbar.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
 
-gamesClick.addEventListener("click", ()=> {
-    window.location.href = "../pages/games.html"
+        //give nav functionality
+        let aboutClick = document.getElementById("about-button")
+        let gamesClick = document.getElementById("games-button")
+        let artClick = document.getElementById("art-button")
+        let miscClick = document.getElementById("misc-button")
+        
+        aboutClick.addEventListener("click", ()=> {
+            window.location.href = "../pages/index.html"
+        })
+
+        gamesClick.addEventListener("click", ()=> {
+            window.location.href = "../pages/games.html"
+        })
+        
+        artClick.addEventListener("click", ()=> {
+            window.location.href = "../pages/art.html"
+        })
+        
+        miscClick.addEventListener("click", ()=> {
+            window.location.href = "../pages/misc.html"
+        })
+    })
 })
 
-artClick.addEventListener("click", ()=> {
-    window.location.href = "../pages/art.html"
-})
-
-miscClick.addEventListener("click", ()=> {
-    window.location.href = "../pages/misc.html"
-})
 
 //adding functionality to social links
 let emailClick = document.getElementById("email");
