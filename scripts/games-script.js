d80d215fd33067a970898d3e1010159e71eedad9
Camille Tsui <camille.tsui@edu.sait.ca>
2026-03-23 19:00:48 -0600

    Refactor game card
    you and just hove about of data in and it will create the card automatically now

---
+++ scripts/games-script.js
@@ -0,0 +1,23 @@
+/* Games Page */
+fetch("../data/game.json")
+.then(res => res.json())
+.then(games => {
+    const container = document.getElementById("games-container");
+
+    games.forEach(game => {
+        container.innerHTML += `
+            <div class="game-card">
+                <h2 class="game-title">${game.title || "no title"}</h2>
+                ${game.image ? `<img src="${game.image}" alt="">` : ""}
+                ${game.description ? `<p>${game.description}</p>` : ""}
+                ${game.date ? `<p class="game-date">${game.date}</p>` : ""}
+                ${game.tags ? `
+                    <ul class="game-tags">
+                    ${game.tags.map(tag => `<li>#${tag}</li>`).join("")}
+                    </ul>
+                ` : ""}
+                </ul>
+            </div>
+        `
+    })
+})
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,40 +31,5 @@ window.addEventListener("DOMContentLoaded", () => {
 })
 
 
-//adding functionality to social links
-let emailClick = document.getElementById("email");
-let phoneClick = document.getElementById("phone")
-let gitClick = document.getElementById("github")
-let itchClick = document.getElementById("itch.io");
-let linkedinClick = document.getElementById("linked-in")
-let youtubeClick = document.getElementById("youtube")
 
-/*copy to clip board*/
-emailClick.addEventListener("click", ()=> {
-    navigator.clipboard.writeText("camilletsui.zt@gmail.com").then(() => {
-        alert ("Email copied!");
-    })
-})
 
-phoneClick.addEventListener("click", ()=> {
-    navigator.clipboard.writeText("825-735-2226").then(() => {
-        alert ("Phone copied!");
-    })
-})
-
-/*redirects*/
-gitClick.addEventListener("click", ()=> {
-    window.location.href = "https://github.com/Sklrat";
-})
-
-itchClick.addEventListener("click", ()=> {
-    window.location.href = "https://camsthingz.itch.io/";
-})
-
-linkedinClick.addEventListener("click", ()=> {
-    window.location.href = "https://www.linkedin.com/in/camille-tsui-6b371a2a1/";
-})
-
-youtubeClick.addEventListener("click", ()=> {
-    window.location.href = "https://www.youtube.com/@CamsThingz";
-})
