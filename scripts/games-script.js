b4d162383c50343dee824c38e03d5739bdf332d9
Camille Tsui <camille.tsui@edu.sait.ca>
2026-03-23 20:49:16 -0600

    Add and change some fonts

--- scripts/games-script.js
+++ scripts/games-script.js
@@ -7,7 +7,7 @@ fetch("../data/game.json")
     games.forEach(game => {
         container.innerHTML += `
             <div class="game-card">
-                <h2 class="game-title">${game.title || "no title"}</h2>
+                <h2 class="game-title">${game.title || "Untitled Game"}</h2>
                 ${game.image ? `<img src="${game.image}" alt="">` : ""}
                 ${game.description ? `<p>${game.description}</p>` : ""}
                 ${game.date ? `<p class="game-date">${game.date}</p>` : ""}
