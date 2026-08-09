fe2b7ac3d25d6aff5d7927421a39e91472dcb67e
Camille Tsui <camsthingz@gmail.com>
2026-08-08 21:58:41 -0600

    all basics for art gallary done

--- scripts/art-script.js
+++ scripts/art-script.js
@@ -1,31 +1,32 @@
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg");
+let savedArts = [];
 
 fetch("../data/art.json")
 .then(res => res.json())
 .then(arts => {
+    savedArts = arts;
     const conatiner = document.querySelector(".art-container");
-    arts.forEach(art => {
+    arts.forEach((art, index) => {
         conatiner.innerHTML += `
-        ${art.image ? `<img src="${art.image}" onclick="openFullImg(this.src)">` : ""}`;
+        ${art.image ? `<img src="${art.image}" onclick="openFullImg(${index})">` : ""}`;
     });
 });
 
-function openFullImg(pic){
+function openFullImg(index){
+    selectedArt = savedArts[index]
+    fullImgBox.innerHTML = "";
     fullImgBox.style.display = "flex";
-    fullImg.src = pic;
-//idk what im doing
-    // fetch("../data/art.json")
-    //     .then(res => res.json())
-    //     .then(art => {
-        
-        
-    //         art.forEach(element => {
-    //             fullImgBox.innerHTML += `
-    //             ${art.title ? `<h2>${art.title}</h2>` : ""}
-    //             `
-    //         });
-    //     })
+    fullImgBox.innerHTML += `
+    ${selectedArt.image ? `<img src=${selectedArt.image} id="fullImg">` : ""}
+    <span onclick="closeFullImg()">X</span>
+    <div class = "art-info">
+    ${selectedArt.title ? `<h1>${selectedArt.title}</h1>` : ""}
+    ${selectedArt.description ? `<p>${selectedArt.description}</p>` : ""}
+    ${selectedArt.date ? `<p>${selectedArt.date}</p>` : ""}
+    </div>
+    `
+    //fullImg.src = selectedArt.image;
 }
 
 function closeFullImg(){
