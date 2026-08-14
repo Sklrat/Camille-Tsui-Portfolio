2972579a64bbd3bb8c0d3560e90bbeaa445048c3
Camille Tsui <camsthingz@gmail.com>
2026-08-14 00:13:46 -0600

    pretty next/prev buttons

--- scripts/art-gallery-script.js
+++ scripts/art-gallery-script.js
@@ -33,8 +33,20 @@ function openFullImg(index){
     ${selectedArt.description ? `<p>${selectedArt.description}</p>` : ""}
     ${selectedArt.date ? `<p>${selectedArt.date}</p>` : ""}
     </div>
-                        <button id="art-button-prev">&#8656;</button>
-                    <button id="art-button-next">&#8658;</button>
+                        <button id="art-button-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <circle cx="12" cy="12" r="10" fill="white"/>
+  <path d="M0 0h24v24H0z" fill="none" />
+  <path fill="currentColor" d="M2 15v2h1v2h1v1h1v1h2v1h2v1h6v-1h2v-1h2v-1h1v-1h1v-2h1v-2h1V9h-1V7h-1V5h-1V4h-1V3h-2V2h-2V1H9v1H7v1H5v1H4v1H3v2H2v2H1v6zm1-6h1V7h1V5h2V4h2V3h6v1h2v1h2v2h1v2h1v6h-1v2h-1v2h-2v1h-2v1H9v-1H7v-1H5v-2H4v-2H3z" />
+  <path fill="currentColor" d="M7 13v-2h1v-1h1V9h1V8h1V7h1v4h6v2h-6v4h-1v-1h-1v-1H9v-1H8v-1z" />
+</svg>
+</button>
+                    <button id="art-button-next">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <circle cx="12" cy="12" r="10" fill="white"/>
+  <path fill="currentColor" d="M22 9V7h-1V5h-1V4h-1V3h-2V2h-2V1H9v1H7v1H5v1H4v1H3v2H2v2H1v6h1v2h1v2h1v1h1v1h2v1h2v1h6v-1h2v-1h2v-1h1v-1h1v-2h1v-2h1V9zm-1 6h-1v2h-1v2h-2v1h-2v1H9v-1H7v-1H5v-2H4v-2H3V9h1V7h1V5h2V4h2V3h6v1h2v1h2v2h1v2h1z"/>
+  <path fill="currentColor" d="M17 11v2h-1v1h-1v1h-1v1h-1v1h-1v-4H6v-2h6V7h1v1h1v1h1v1h1v1z"/>
+</svg>
+</button>
     `
 
     /*making buttons work*/
