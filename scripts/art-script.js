c282bf19a0ec43cbe017b6fad018fb9aa53ea162
Camille Tsui <camsthingz@gmail.com>
2026-09-16 21:06:47 -0600

    Create technical art page

--- scripts/art-script.js
+++ scripts/art-script.js
@@ -1,3 +1,7 @@
 function goToArtPage(id) {
     window.location.href = `../pages/art-gallery.html?id=${id}`
 }
+
+function goToTechnicalArtPage() {
+    window.location.href = `../pages/technical-art.html`
+}
